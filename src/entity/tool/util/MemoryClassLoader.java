587b900e819fa7c497ea0ea23987420663ffb59c
linlurui <rockylin@qq.com>
2023-12-29 23:45:14 +0800

    release

--- src/entity/tool/util/MemoryClassLoader.java
+++ src/entity/tool/util/MemoryClassLoader.java
@@ -1,13 +1,3 @@
-/**
- *
- *  License: http://www.apache.org/licenses/LICENSE-2.0
- *  Home page: https://github.com/linlurui/ccweb
- *  Note: to build on java, include the jdk1.8+ compiler symbol (and yes,
- *  I know the difference between language and runtime versions; this is a compromise).
- * @author linlurui
- * @Date Date: 2019-02-10
- */
-
 package entity.tool.util;
 
 import java.net.URL;
